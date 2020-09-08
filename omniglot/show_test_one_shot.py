c044e588aad4af42db37a9e293b4f84ab18497d9
Jiang Hao <13153633577@163.com>
2020-09-08 22:41:05 +0800

    add show file

---
+++ omniglot/show_test_one_shot.py
@@ -0,0 +1,185 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.optim.lr_scheduler import StepLR
+import numpy as np
+import task_generator as tg
+import os
+import math
+import argparse
+import random
+import scipy as sp
+import scipy.stats
+
+# Hyper Parameters
+FEATURE_DIM = 64  # 论文使用64层提取特征
+RELATION_DIM = 8  # 输出得分最后一层为8
+CLASS_NUM = 5  # 5way
+SAMPLE_NUM_PER_CLASS = 1  # 1shot
+BATCH_NUM_PER_CLASS = 19  # 其余19张作为查询集 Q
+EPISODE = 1000000
+TEST_EPISODE = 1000
+LEARNING_RATE = 0.001
+GPU = 0
+# HIDDEN_UNIT = 10
+
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0*np.array(data)
+    n = len(a)
+    m, se = np.mean(a),scipy.stats.sem(a)
+    h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
+    return m,h
+
+class CNNEncoder(nn.Module):
+    """docstring for ClassName"""
+    def __init__(self):
+        super(CNNEncoder, self).__init__()
+        self.layer1 = nn.Sequential(
+                        nn.Conv2d(1,64,kernel_size=3,padding=0),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=0),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU())
+        self.layer4 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU())
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        #out = out.view(out.size(0),-1)
+        return out # 64
+
+class RelationNetwork(nn.Module):
+    """docstring for RelationNetwork"""
+    def __init__(self,input_size,hidden_size):
+        super(RelationNetwork, self).__init__()
+        self.layer1 = nn.Sequential(
+                        nn.Conv2d(128,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(
+                        nn.Conv2d(64,64,kernel_size=3,padding=1),
+                        nn.BatchNorm2d(64, momentum=1, affine=True),
+                        nn.ReLU(),
+                        nn.MaxPool2d(2))
+        self.fc1 = nn.Linear(input_size,hidden_size)
+        self.fc2 = nn.Linear(hidden_size,1)
+
+    def forward(self,x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0),-1)
+        out = F.relu(self.fc1(out))
+        out = torch.sigmoid(self.fc2(out))
+        return out
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        m.weight.data.normal_(0, math.sqrt(2. / n))
+        if m.bias is not None:
+            m.bias.data.zero_()
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+    elif classname.find('Linear') != -1:
+        n = m.weight.size(1)
+        m.weight.data.normal_(0, 0.01)
+        m.bias.data = torch.ones(m.bias.data.size())
+
+def main():
+    # Step 1: init data folders
+    print("init data folders")
+    # init character folders for dataset construction
+    metatrain_character_folders,metatest_character_folders = tg.omniglot_character_folders()
+
+    # Step 2: init neural networks
+    print("init neural networks")
+
+    feature_encoder = CNNEncoder()
+    relation_network = RelationNetwork(FEATURE_DIM,RELATION_DIM)
+
+
+    feature_encoder.cuda(GPU)
+    relation_network.cuda(GPU)
+
+    feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(),lr=LEARNING_RATE)
+    feature_encoder_scheduler = StepLR(feature_encoder_optim,step_size=100000,gamma=0.5)
+    relation_network_optim = torch.optim.Adam(relation_network.parameters(),lr=LEARNING_RATE)
+    relation_network_scheduler = StepLR(relation_network_optim,step_size=100000,gamma=0.5)
+
+    if os.path.exists(str("./models/omniglot_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
+        feature_encoder.load_state_dict(torch.load(str("./models/omniglot_feature_encoder_" + str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")))
+        print("load feature encoder success")
+    if os.path.exists(str("./models/omniglot_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")):
+        relation_network.load_state_dict(torch.load(str("./models/omniglot_relation_network_"+ str(CLASS_NUM) +"way_" + str(SAMPLE_NUM_PER_CLASS) +"shot.pkl")))
+        print("load relation network success")
+
+    total_accuracy = 0.0
+    for episode in range(EPISODE):
+
+
+            # test
+            print("Testing...")
+            total_rewards = 0
+            accuracies = []
+            for i in range(TEST_EPISODE):
+                degrees = random.choice([0,90,180,270])
+                task = tg.OmniglotTask(metatest_character_folders,CLASS_NUM,SAMPLE_NUM_PER_CLASS,SAMPLE_NUM_PER_CLASS,)
+                sample_dataloader = tg.get_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="train",shuffle=False,rotation=degrees)
+                test_dataloader = tg.get_data_loader(task,num_per_class=SAMPLE_NUM_PER_CLASS,split="test",shuffle=True,rotation=degrees)
+
+                sample_images,sample_labels = sample_dataloader.__iter__().next()
+                test_images,test_labels = test_dataloader.__iter__().next()
+
+                # calculate features
+                sample_features = feature_encoder(Variable(sample_images).cuda(GPU)) # 5x64
+                test_features = feature_encoder(Variable(test_images).cuda(GPU)) # 20x64
+
+                # calculate relations
+                # each batch sample link to every samples to calculate relations
+                # to form a 100x128 matrix for relation network
+                sample_features_ext = sample_features.unsqueeze(0).repeat(SAMPLE_NUM_PER_CLASS*CLASS_NUM,1,1,1,1)
+                test_features_ext = test_features.unsqueeze(0).repeat(SAMPLE_NUM_PER_CLASS*CLASS_NUM,1,1,1,1)
+                test_features_ext = torch.transpose(test_features_ext,0,1)
+
+                relation_pairs = torch.cat((sample_features_ext,test_features_ext),2).view(-1,FEATURE_DIM*2,5,5)
+                relations = relation_network(relation_pairs).view(-1,CLASS_NUM)
+
+                _,predict_labels = torch.max(relations.data,1)
+
+                rewards = [1 if predict_labels[j]==test_labels[j] else 0 for j in range(CLASS_NUM)]
+
+                total_rewards += np.sum(rewards)
+                accuracy = np.sum(rewards)/1.0/CLASS_NUM/SAMPLE_NUM_PER_CLASS
+                accuracies.append(accuracy)
+
+            test_accuracy,h = mean_confidence_interval(accuracies)
+
+            print("test accuracy:",test_accuracy,"h:",h)
+            total_accuracy += test_accuracy
+
+    print("aver_accuracy:",total_accuracy/EPISODE)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
